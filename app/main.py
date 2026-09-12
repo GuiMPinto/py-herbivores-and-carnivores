b773dc3220bfaaece4ef948b838945b4c230be6d
PintoJR <guimoraespinto@gmail.com>
2026-09-12 01:24:02 -0300

    Solution02

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
 class Animal:
-    alive = []  # lista de todos os animais vivos
-    
-    def __init__(self, name):
+    alive = []
+
+    def __init__(self, name: str) -> None:
         self.name = name
         self.health = 100
         self.hidden = False
         Animal.alive.append(self)
 
+    @classmethod
     def __repr__(self):
-        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+        return f"{{Name: {self.name}, Health: {self.health}, \
+                   Hidden: {self.hidden}}}"
     
-
 class Herbivore(Animal):
     def hide(self):
         self.hidden = True
@@ -20,7 +21,8 @@ class Сarnivore(Animal):
     def bite(self, herbivore):
         if not herbivore.hidden:
             herbivore.health -= 50
-            print(f"{self.name} bit {herbivore.name}. {herbivore.name}'s health is now {herbivore.health}.")
+            print(f"{self.name} bit {herbivore.name}\
+                  . {herbivore.name}'s health is now {herbivore.health}.")
             if herbivore.health <= 0:
                 print(f"{herbivore.name} has died.")
                 Animal.alive.remove(herbivore)
