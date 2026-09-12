fd0605f368105ba1edfa0234be12297a5b2a442f
PintoJR <guimoraespinto@gmail.com>
2026-09-12 01:59:49 -0300

    Solution04

--- app/main.py
+++ app/main.py
@@ -8,26 +8,22 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self):
-        return f"{{Name: {self.name}, Health: {self.health}, \
-                   Hidden: {self.hidden}}}"
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
     
 class Herbivore(Animal):
     def hide(self):
         self.hidden = not self.hidden
         # print(f"{self.name} is hiding.")   
 
-class Сarnivore(Animal):
+class Carnivore(Animal):
     def bite(self, animal):
         if isinstance(animal, Carnivore):
-            print("Cannot bite another carnivore!")
             return
         elif isinstance(animal, Herbivore):
             if animal.hidden:
-                print(f"{animal.name} is hiding and cannot be bitten.")
+                return
             else:
                 animal.health -= 50
-                print(f"{self.name} bit {animal.name}. {animal.name}'s health is now {animal.health}.")
                 if animal.health <= 0:
-                    print(f"{animal.name} has died.")
                     Animal.alive.remove(animal)
-        
+                    return
