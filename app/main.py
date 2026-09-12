585d1abff47af55e5006d00d9ff607a3d3c6caae
PintoJR <guimoraespinto@gmail.com>
2026-09-12 02:55:51 -0300

    Solution08

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, health: int | None = None) -> None:
         self.name = name
-        self.health = 100
+        self.health = health if health is not None else 100
         self.hidden = False
         Animal.alive.append(self)
+
 
     def __repr__(self) -> str:
         return (f"{{Name: {self.name}, Health: {self.health}, "
